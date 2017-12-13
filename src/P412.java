26374477e648c3d7b333180877ebeb945bd23181
joe <736777445@qq.com>
2017-12-13 22:17:55 +0800

    新刷412

---
+++ src/P412.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Joe on 2017/12/13.
+ * https://leetcode.com/problems/fizz-buzz/description/
+ */
+public class P412 {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>(n);
+
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                list.add("Fizz");
+            } else if (i % 5 == 0) {
+                list.add("Buzz");
+            } else {
+                list.add(String.valueOf(i));
+            }
+        }
+
+        return list;
+    }
+}
