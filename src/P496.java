82e41c31bf256ba26f8ce1fadf43b09e52ed6bfa
joe <736777445@qq.com>
2017-12-18 22:24:54 +0800

    p496 next-greater-element-i

---
+++ src/P496.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Created by Joe on 2017/12/18.
+ * https://leetcode.com/problems/next-greater-element-i/description/
+ */
+public class P496 {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>(); // map from x to next greater element of x
+        Stack<Integer> stack = new Stack<>();
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num)
+                map.put(stack.pop(), num);
+            stack.push(num);
+        }
+        for (int i = 0; i < findNums.length; i++)
+            findNums[i] = map.getOrDefault(findNums[i], -1);
+        return findNums;
+    }
+}
