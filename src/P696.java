6f43725ef9e6afaae7777ffb1bc51e88a63ba152
joe <736777445@qq.com>
2018-01-04 23:00:55 +0800

    696. Count Binary Substrings

---
+++ src/P696.java
@@ -0,0 +1,27 @@
+/**
+ * Created by Joe on 2018/1/4.
+ */
+public class P696 {
+    public int countBinarySubstrings(String s) {
+        int[] groups = new int[s.length()];
+        int t = 0;
+        groups[0] = 1;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i-1) != s.charAt(i)) {
+                groups[++t] = 1;
+            } else {
+                groups[t]++;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 1; i <= t; i++) {
+            ans += Math.min(groups[i-1], groups[i]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        new P696().countBinarySubstrings("00110");
+    }
+}
