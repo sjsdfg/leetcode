fe0f4e722397d908e9f2aa76f588d970a2f1e4b8
joe <736777445@qq.com>
2018-01-18 22:28:20 +0800

    733. Flood Fill

---
+++ src/P733.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Joe on 2018/1/18.
+ * 733. Flood Fill
+ * https://leetcode.com/problems/flood-fill/description/
+ */
+public class P733 {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) return image;
+        fill(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+
+    private void fill(int[][] image, int sr, int sc, int color, int newColor) {
+        if (sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) return;
+        image[sr][sc] = newColor;
+        fill(image, sr + 1, sc, color, newColor);
+        fill(image, sr - 1, sc, color, newColor);
+        fill(image, sr, sc + 1, color, newColor);
+        fill(image, sr, sc - 1, color, newColor);
+    }
+}
