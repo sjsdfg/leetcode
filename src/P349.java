710b7a5c1a07097e4fcf3e18048987e5e1e1205b
joe <736777445@qq.com>
2018-01-23 22:36:07 +0800

    349. Intersection of Two Arrays

---
+++ src/P349.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Joe on 2018/1/23.
+ * 349. Intersection of Two Arrays
+ * https://leetcode.com/problems/intersection-of-two-arrays/description/
+ */
+public class P349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> intersect = new HashSet<>();
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            if (set.contains(nums2[i])) {
+                intersect.add(nums2[i]);
+            }
+        }
+        int[] result = new int[intersect.size()];
+        int i = 0;
+        for (Integer num : intersect) {
+            result[i++] = num;
+        }
+        return result;
+    }
+}
