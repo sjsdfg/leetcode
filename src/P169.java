0ed7e3902793d073dd90ebf7d427b7d23ebf3460
joe <736777445@qq.com>
2018-01-24 23:19:39 +0800

    169. Majority Element

--- src/P169.java
+++ src/P169.java
@@ -6,14 +6,14 @@
 public class P169 {
     public int majorityElement(int[] num) {
 
-        int major=num[0], count = 1;
-        for(int i=1; i<num.length;i++){
-            if(count==0){
+        int major = num[0], count = 1;
+        for (int i = 1; i < num.length; i++) {
+            if (count == 0) {
                 count++;
-                major=num[i];
-            }else if(major==num[i]){
+                major = num[i];
+            } else if (major == num[i]) {
                 count++;
-            }else count--;
+            } else count--;
 
         }
         return major;
