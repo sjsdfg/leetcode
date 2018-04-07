1d6a3ee2498e5b14f5a3456a0be69cb12aae2155
sjsdfg <736777445@qq.com>
2018-04-07 15:59:27 +0800

    766. Toeplitz Matrix

--- src/P766.java
+++ src/P766.java
@@ -8,12 +8,13 @@ public class P766 {
         int m = matrix.length;
         int n = matrix[0].length;
 
+        //遍历第一列
         for (int i = 0; i < m; i++) {
             if (!check(matrix, i, 0, m, n)) {
                 return false;
             }
         }
-
+        //遍历第一行
         for (int i = 0; i < n; i++) {
             if (!check(matrix, 0, i, m, n)) {
                 return false;
@@ -24,7 +25,10 @@ public class P766 {
     }
 
     public boolean check(int[][] matrix, int i, int j, int m, int n) {
+        //边界判断
         if (i + 1 < m && j + 1 < n) {
+            //判断和下一个值是否相同
+            //相同则继续判断
             return matrix[i][j] == matrix[i + 1][j + 1] && check(matrix, i + 1, j + 1, m, n);
         }
         return true;
